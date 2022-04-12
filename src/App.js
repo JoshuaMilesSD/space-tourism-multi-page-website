303402f18797cd93ae5d3e2e7728588be7b32f09
JoshuaMilesSD <joshmiles2010@gmail.com>
2022-04-11 17:14:35 -0700

    commit on main

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import {
-  Header,
-  Home,
-  Destination,
-  Crew,
-  Technology,
-  Error,
-} from "./components"
+import React from "react";
 
-function App() {
+export default function App(){
   return (
-    <Router>
-      <main>
-        <Header />
-
-        <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/destination">
-            <Destination />
-          </Route>
-          <Route path="/crew">
-            <Crew />
-          </Route>
-          <Route path="/technology">
-            <Technology />
-          </Route>
-          <Route path="*">
-            <Error />
-          </Route>
-        </Switch>
-      </main>
-    </Router>
+  <>
+  <h1>Hello world</h1>
+  </>
   )
 }
-
-export default App
